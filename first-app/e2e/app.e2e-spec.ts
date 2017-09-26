084810b391389812cee005df98603165e9e4948e
AldridgeDev <aldridge.developer@gmail.com>
2017-09-25 23:18:12 -0600

    section 2 lecture 28

--- first-app/e2e/app.e2e-spec.ts
+++ first-app/e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { AppPage } from './app.po';
+import { FirstAppPage } from './app.po';
 
-describe('first-app App', () => {
-  let page: AppPage;
+describe('first-app App', function() {
+  let page: FirstAppPage;
 
   beforeEach(() => {
-    page = new AppPage();
+    page = new FirstAppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('app works!');
   });
 });
