d89e8a3b7b765602e70f3ba95893619e823252c1
AldridgeDev <aldridge.developer@gmail.com>
2017-10-07 21:52:04 -0600

    getting an error

--- cms/src/app/messages/message-edit/message-edit.component.ts
+++ cms/src/app/messages/message-edit/message-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-edit',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message-edit.component.css']
 })
 export class MessageEditComponent implements OnInit {
+  currentSender: string = 'Antonio';
+  @ViewChild('subject') subjectRef: ElementRef;
+  @ViewChild('msgText') msgRef: ElementRef;
+  @Output() addMessage = new EventEmitter<Message>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSendMessage(){
+    const ingSubject = this.subjectRef.nativeElement.value;
+    const ingMsgText = this.subjectRef.nativeElement.value;
+    const newMsg = new Message(this.currentSender, ingSubject, ingMsgText);
+      this.addMessage.emit(newMsg);
+  }
+
+  onClear() {
+    this.subjectRef.nativeElement.value = '';
+    this.msgRef.nativeElement.value = '';
+  }
 }
