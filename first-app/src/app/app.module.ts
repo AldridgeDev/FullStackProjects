f699dba8388b8918e7274330a27975fd00c79afb
AldridgeDev <aldridge.developer@gmail.com>
2017-10-19 10:36:11 -0600

    updating

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
 
 @NgModule({
   declarations: [
@@ -22,9 +23,9 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     ShoppingEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
-  providers: [],
+  providers: [ShoppingListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- first-app/src/app/recipes/recipe.service.ts
+++ first-app/src/app/recipes/recipe.service.ts
@@ -1,9 +1,25 @@
+import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://c1.staticflickr.com/9/8575/15775087589_a725c0b077_b.jpg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://c1.staticflickr.com/9/8575/15775087589_a725c0b077_b.jpg')
+    new Recipe('A Test Recipe',
+      'This is simply a test',
+      'https://c1.staticflickr.com/9/8575/15775087589_a725c0b077_b.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe('Another Test Recipe',
+       'This is simply a test',
+       'https://c1.staticflickr.com/9/8575/15775087589_a725c0b077_b.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ])
   ];
 
   getRecipes() {
