79a53a8bc35df7b9fdf8d16f49b1c0d06437a647
AldridgeDev <aldridge.developer@gmail.com>
2017-10-24 17:23:50 -0600

    updating

--- cms/src/app/contacts/contact-list/contact-list.component.ts
+++ cms/src/app/contacts/contact-list/contact-list.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Injectable } from '@angular/core';
 import { Contact } from '../contact.model';
+import { ContactService } from '../contact.service';
 
+@Injectable()
 @Component({
   selector: 'cms-contact-list',
   templateUrl: './contact-list.component.html',
@@ -8,25 +10,28 @@ import { Contact } from '../contact.model';
 })
 export class ContactListComponent implements OnInit {
   @Output() contactWasSelected = new EventEmitter<Contact>();
-  contacts: Contact[] = [
-    new Contact(
-      '1',
-      'Bro. Jackson',
-      'jacksonk@byui.edu',
-      '208-496-3771',
-      'https://web.byui.edu/Directory/Employee/jacksonk.jpg',
-      null),
+  public contacts: Contact[] = [];
+  // contacts: Contact[] = [
+  //   new Contact(
+  //     '1',
+  //     'Bro. Jackson',
+  //     'jacksonk@byui.edu',
+  //     '208-496-3771',
+  //     'https://web.byui.edu/Directory/Employee/jacksonk.jpg',
+  //     null),
+  //
+  //   new Contact(
+  //     '2',
+  //     'Bro. Barzee',
+  //     'barzeer@byui.edu',
+  //     '208-496-3768',
+  //     'https://web.byui.edu/Directory/Employee/barzeer.jpg',
+  //     null)
+  //   ];
 
-    new Contact(
-      '2',
-      'Bro. Barzee',
-      'barzeer@byui.edu',
-      '208-496-3768',
-      'https://web.byui.edu/Directory/Employee/barzeer.jpg',
-      null)
-    ];
-
-  constructor() { }
+  constructor(private contactService: ContactService) {
+    this.contacts = this.contactService.getContacts();
+  }
 
   ngOnInit() {
   }
